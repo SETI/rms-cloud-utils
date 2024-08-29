261cac741fa210ced26a384549f1709c780e6728
Robert French <rfrench@seti.org>
2024-08-29 22:16:59 +0000

    Add sync version, improve error handling

---
+++ update_holdings/update_one_archive_volset_async.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+set -e
+
+if [ $# != 2 ] && [ $# != 3 ]; then
+    echo "Usage: update_one_archive_volset.sh <type> <volset> [--force]"
+    exit -1
+fi
+
+if [ $# == 3 ] && [ "$3" != "--force" ]; then
+    echo "Usage: update_one_archive_volset.sh <type> <volset> [--force]"
+    exit -1
+fi
+
+TYPE=$1
+VOLSET=$2
+FORCE=$3
+
+SUFFIX=
+if [[ $TYPE != "volumes" ]]; then
+    SUFFIX=_$TYPE
+fi
+
+TARGZ=${SUFFIX}.tar.gz
+
+echo "** Updating all volumes of type ${TYPE} in volset ${VOLSET}"
+
+mkdir -p logs
+
+for ARCHIVE_PATH in $(gsutil ls gs://rms-node/holdings/archives-${TYPE}/${VOLSET}); do
+    ARCHIVE_FILE=$(basename ${ARCHIVE_PATH})
+    VOLUME=${ARCHIVE_FILE%"${TARGZ}"}
+    echo Updating volume ${TYPE}/${VOLSET}/${VOLUME}
+    ./update_one_archive_volume.sh ${TYPE} ${VOLSET} ${VOLUME} ${FORCE} > logs/${TYPE}_${VOLSET}_${VOLUME} 2>&1 &
+done
--- update_holdings/update_one_archive_volume.sh
+++ update_holdings/update_one_archive_volume.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
-set -e
-
 if [ $# != 3 ] && [ $# != 4 ]; then
     echo "Usage: update_one_archive_volume.sh <type> <volset> <volume> [--force]"
     exit -1
@@ -25,17 +23,44 @@ ARCHIVE_FILE=${VOLUME}${SUFFIX}.tar.gz
 
 if [ -z "$FORCE" ]; then
     set +e
-    gsutil ls gs://rms-node-holdings/pds3-holdings/archives-${TYPE}/${VOLSET}/${ARCHIVE_FILE}
+    gsutil ls gs://rms-node-holdings/pds3-holdings/archives-${TYPE}/${VOLSET}/${ARCHIVE_FILE} > /dev/null 2>&1
     if [ $? == 0 ]; then
-        echo Skipping ${TYPE}/${VOLSET}/${ARCHIVE_FILE} because copy already exists
+        echo Skipping ${TYPE}/${VOLSET}/${ARCHIVE_FILE} because archive file already exists
         exit 0
     fi
     set -e
 fi
 
+echo "** gsutil cp gs://rms-node/holdings/archives-${TYPE}/${VOLSET}/${ARCHIVE_FILE} ."
 gsutil cp gs://rms-node/holdings/archives-${TYPE}/${VOLSET}/${ARCHIVE_FILE} .
+if [ $? -ne 0 ]; then
+    echo "FAIL: gsutil cp gs://rms-node/holdings/archives-${TYPE}/${VOLSET}/${ARCHIVE_FILE} ."
+    rm -f ${ARCHIVE_FILE}
+    rm -rf ${VOLUME}
+    exit 1
+fi
 tar xf ${ARCHIVE_FILE}
+if [ $? -ne 0 ]; then
+    echo "FAIL: tar xf ${ARCHIVE_FILE}"
+    rm -f ${ARCHIVE_FILE}
+    rm -rf ${VOLUME}
+    exit 1
+fi
 gsutil cp ${ARCHIVE_FILE} gs://rms-node-holdings/pds3-holdings/archives-${TYPE}/${VOLSET}/${ARCHIVE_FILE}
+if [ $? -ne 0 ]; then
+    echo "FAIL: gsutil cp ${ARCHIVE_FILE} gs://rms-node-holdings/pds3-holdings/archives-${TYPE}/${VOLSET}/${ARCHIVE_FILE}"
+    rm -f ${ARCHIVE_FILE}
+    rm -rf ${VOLUME}
+    exit 1
+fi
 gsutil -m rsync -r ${VOLUME} gs://rms-node-holdings/pds3-holdings/${TYPE}/${VOLSET}/${VOLUME}
+if [ $? -ne 0 ]; then
+    echo "FAIL: gsutil -m rsync -r ${VOLUME} gs://rms-node-holdings/pds3-holdings/${TYPE}/${VOLSET}/${VOLUME}"
+    rm -f ${ARCHIVE_FILE}
+    rm -rf ${VOLUME}
+    exit 1
+fi
 rm ${ARCHIVE_FILE}
 rm -rf ${VOLUME}
+
+echo "SUCCESS"
