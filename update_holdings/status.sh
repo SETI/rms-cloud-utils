307506eda4b71d8bff6a1f9e01ffdad15756c923
Robert S. French <rfrench@seti.org>
2024-09-03 00:39:10 +0000

    Add status.sh

---
+++ update_holdings/status.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+TOTAL=`ls logs|wc -l`
+SUCCESS=`grep SUCCESS logs/*|wc -l`
+FAIL=`grep FAIL logs/* |wc -l`
+echo "Total:  " $TOTAL
+echo "Success:" $SUCCESS
+echo "Fail:   " $FAIL
+echo "Neither:" `expr $TOTAL - $SUCCESS - $FAIL`
